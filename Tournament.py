b3cbc30d080831b36a62c6eea1a179fc07038d00
eng.eduardoapg <eng.eduardoapg@gmail.com>
2023-04-30 01:31:54 -0300

    :sparkles: classe que realiza o torneio de uma população binária

---
+++ Tournament.py
@@ -0,0 +1,86 @@
+import random
+import numpy
+
+
+class Tournament:
+    def __init__(self, selection, performance, population):
+        self.population = population.data
+        self.selection = selection
+        self.performance = performance
+        self = self.compete()
+
+    @property
+    def new_population(self):
+        return random.sample(self.winner_list, self.selection.number_of_tournaments())
+
+    def compete(self):
+        for participants in self.selection.tournament_sample():
+            self.participants = participants
+            self.first_participant = self.participants
+            self.second_participant = self.participants
+            self.winner_list_performance = self.matchperformance()
+            self.winner_list = self.winner()
+
+    def winner(self):
+        return self.population[self.who_is_winner]
+
+    def matchperformance(self):
+        return self.performance[self.who_is_winner]
+
+    @property
+    def participants(self):
+        return self.__participants
+
+    @participants.setter
+    def participants(self, new_participants):
+        self.__participants = new_participants
+        return self
+
+    @property
+    def winner_list_performance(self):
+        return self.__winner_list_performance
+
+    @winner_list_performance.setter
+    def winner_list_performance(self, performance_of_winner):
+        if hasattr(self, 'winner_list_performance') == False:
+            self.__winner_list_performance = [performance_of_winner]
+        else:
+            self.__winner_list_performance.append(performance_of_winner)
+            return self
+
+    @property
+    def winner_list(self):
+        return self.__winner_list
+
+    @winner_list.setter
+    def winner_list(self, winner):
+        if hasattr(self, 'winner_list') == False:
+            self.__winner_list = [winner]
+        else:
+            self.__winner_list.append(winner)
+        return self
+
+    @property
+    def first_participant(self):
+        return self.__first_participant
+
+    @first_participant.setter
+    def first_participant(self, participants):
+        self.__first_participant = self.performance[participants[0]]
+        return self
+
+    @property
+    def second_participant(self):
+        return self.__second_participant
+
+    @second_participant.setter
+    def second_participant(self, participants):
+        self.__second_participant = self.performance[participants[1]]
+        return self
+
+    @property
+    def who_is_winner(self):
+        if self.first_participant < self.second_participant:
+            return self.participants[0]
+        else:
+            return self.participants[1]
