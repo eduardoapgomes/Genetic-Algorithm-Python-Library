9c7c1586c578f34461eb6bd40f5603e274b15be2
eng.eduardoapg <eng.eduardoapg@gmail.com>
2023-05-01 17:38:22 -0300

    :rocket: código main é capaz de gerar uma população inicial e aleatória

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from math import inf
 from mimetypes import init
 from operator import le
 from Population import Population
@@ -10,35 +11,48 @@ from Tournament import Tournament
 from Fitness import Fitness
 import numpy as np
 import random
+import matplotlib.pyplot as plt
 
 
-Xb = ['01010011100',  # population
-      '11111101010',
-      '01110110110',
-      '11010110111']
+def minimize(data):
+    # Função de minimização
+    return -int(data, 2)/255
 
 
-def fitness(data):
-    return 525
+Pc = 0.6   # probabilidade de crossover
+Pm = 0.01  # probabilidade de mutação
 
+# Definindo a função fitness
+fitness = Fitness(minimize)
 
-Pc = 0.4
-Pm = 0.01
-performance = [0.1, 0.7, 0.9, 1.0]
+# Gerando a população inicial e desempenho inicial
+population = Population.generate(100, 8)
+performance = fitness.evaluate(population)
 
+# Algoritmo Genético
+iter = 0
+x_opt = np.inf
+while iter < 1000:
+    # Parametrizando operações do AG
+    selection = Selection(population, Pc)
+    tournament = Tournament(selection, performance, population)
+    crossover = Crossover(population, selection)
 
-population = Population(Xb)
-selection = Selection(population, Pc)
-tournament = Tournament(selection, performance, population)
-crossover = Crossover(population, selection)
+    # Atualizando a população(torneio, crossover e mutation)
+    tournament = tournament.new_population
+    crossover = crossover.new_population
+    mutants = Mutation(Pm, crossover)
+    mutants = mutants.new_population
 
-# Atualizando a população(torneio, crossover e mutation)
-tournament = tournament.new_population
-crossover = crossover.new_population
-mutants = Mutation(Pm, crossover)
-mutants = mutants.new_population
-# Nova geração
-new_population = tournament + mutants
+    # Nova geração
+    new_population = tournament + mutants
+    performance = fitness.evaluate(new_population)
 
+    # Solução ótima
+    x_arg_min = np.argmin(performance)
+    if performance[x_arg_min] < x_opt:
+        x_opt, x_min = performance[x_arg_min], new_population.data[x_arg_min]
 
-print(new_population)
+    # proxima iteração
+    population = new_population
+    iter += 1
