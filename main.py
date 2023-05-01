5add5d2bb98d064cca4ec5a97646fe33a5a3265c
eng.eduardoapg <eng.eduardoapg@gmail.com>
2023-05-01 19:13:37 -0300

    :rocket: adiciona no algoritmo genético a avaliação de minimização através da classe Fitness.

--- main.py
+++ main.py
@@ -16,26 +16,27 @@ import matplotlib.pyplot as plt
 
 def minimize(data):
     # Função de minimização
-    return -int(data, 2)/255
+    return -((int(data, 2)/(2**30 - 1))**2)
 
 
-Pc = 0.6   # probabilidade de crossover
+Pc = 0.70  # probabilidade de crossover
 Pm = 0.01  # probabilidade de mutação
 
 # Definindo a função fitness
 fitness = Fitness(minimize)
 
 # Gerando a população inicial e desempenho inicial
-population = Population.generate(100, 8)
-performance = fitness.evaluate(population)
+population = Population.generate(100, 30)
+fitness = fitness.evaluate(population)
 
 # Algoritmo Genético
 iter = 0
-x_opt = np.inf
+fitness_history = {'iter': [], 'fitness': [], 'xmin': []}
 while iter < 1000:
+    print(f'''iter: {iter}''')
     # Parametrizando operações do AG
     selection = Selection(population, Pc)
-    tournament = Tournament(selection, performance, population)
+    tournament = Tournament(selection, fitness, population)
     crossover = Crossover(population, selection)
 
     # Atualizando a população(torneio, crossover e mutation)
@@ -46,13 +47,22 @@ while iter < 1000:
 
     # Nova geração
     new_population = tournament + mutants
-    performance = fitness.evaluate(new_population)
+    fitness = fitness.evaluate(new_population)
 
-    # Solução ótima
-    x_arg_min = np.argmin(performance)
-    if performance[x_arg_min] < x_opt:
-        x_opt, x_min = performance[x_arg_min], new_population.data[x_arg_min]
+    # Storing data
+    fitness_iter = {'iter': iter, 'fitness': fitness.min, 'xmin': fitness.xmin}
+    for key, value in fitness_iter.items():
+        fitness_history[key].append(value)
 
     # proxima iteração
     population = new_population
     iter += 1
+
+plt.figure()
+plt.plot(fitness_history['iter'],fitness_history['fitness'],'-o')
+plt.xlabel('Iter (k)')
+plt.ylabel('Fitness($x_{min}$)')
+plt.grid()
+plt.show()
+print('done')
+
